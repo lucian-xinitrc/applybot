0a37d4439a3d583051d5467f83c73c83e23c7344
lucian-xinitrc <cusmirlucian@gmail.com>
2026-02-21 16:19:31 +0000

    New commit

--- bot.py
+++ bot.py
@@ -26,8 +26,10 @@ class ResponseApplyEmbed(disnake.ui.View):
         	await member.add_roles(role)
 
         	await interaction.response.send_message(f"{member.mention} You've been accepted", ephemeral=True)
-		else:
-			await interaction.response.send_message("{self.username} is not on server, or he put wrong username", ephemeral=True)
+
+        else:
+
+        	await interaction.response.send_message("{self.username} is not on server, or he put wrong username", ephemeral=True)
     @disnake.ui.button(label="Deny", style=disnake.ButtonStyle.danger, custom_id="deny_btn")
     async def button_deny_callback(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
     	member = disnake.utils.find( lambda m: m.name == self.username or m.display_name == self.username, interaction.guild.members)
