044a7596b43523ab72df2a1710e089b7fb06c4f9
lucian-xinitrc <cusmirlucian@gmail.com>
2026-02-21 00:37:28 +0000

    New commit

--- bot.py
+++ bot.py
@@ -9,6 +9,74 @@ bot = commands.Bot(
 	intents=disnake.Intents.all(), 
 	allowed_mentions=disnake.AllowedMentions(everyone=True)
 )
+
+last_cache = None
+
+def decrypt(ciphertext_b64):
+	    cipher = AES.new(os.getenv('encryption_key').encode(), AES.MODE_ECB)
+	    decrypted = cipher.decrypt(base64.b64decode(ciphertext_b64))
+	    pad = decrypted[-1]
+	    return decrypted[:-pad].decode('utf-8')
+
+@tasks.loop(seconds=2)
+async def watcher():
+	global last_cache
+	db = os.getenv('db_url')
+	conn = psycopg2.connect(db)
+	cur = conn.cursor()
+
+	cur.execute("cur.execute("""
+    SELECT id, username, first, second, third, fourth,
+           fifth, sixth, seventh, eighth, nineth, tenth
+    FROM public.last_message
+    ORDER BY id DESC
+    LIMIT 1
+""")")
+	row = cur.fetchone()
+
+	cur.close()
+	conn.close()
+	if not row:
+	    return
+
+	if row != last_cache:
+	    last_cache = row
+
+	    (max_id, username, first, second, third, fifth, sixth, seventh, eighth, nineth, tenth) = row
+	    channel = bot.get_channel(1469673733580128431)
+	    decryptedMsg = decrypt(msg)
+	    check = True
+	    if channel:
+	        for word in ["login", "register", "msg", "/give"]:
+	        	if word in decryptedMsg:
+	        		check = False
+	        		break
+	        if check:
+	        	embed = disnake.Embed(
+				    title="Application",
+				    url="https://adarealm.gethonis.com",
+				    color=disnake.Colour.blue(),
+				    timestamp=datetime.datetime.now(),
+				)
+
+				embed.add_field(name="Username", value=username, inline=False)
+
+				embed.add_field(name="What is your Minecraft username?", value=decrypt(first), inline=False)
+				embed.add_field(name="What Mineraft do you use?", value=decrypt(second), inline=False)
+				embed.add_field(name="How old are you?", value=decrypt(third), inline=False)
+				embed.add_field(name="What is your timezone?", value=decrypt(fourth), inline=False)
+				embed.add_field(name="Why do you want to join our community?", value=decrypt(fifth), inline=False)
+
+				embed.add_field(name="Are you okay with a 2-day trial period before becoming a full member?", value=decrypt(sixth), inline=False)
+				embed.add_field(name="What are your main interests if you do join the server and what would you bring to our community?", value=decrypt(seventh), inline=False)
+				embed.add_field(name="What does a 'healthy community' mean to you?", value=decrypt(eighth), inline=False)
+				embed.add_field(name="Tell us a little bit about yourself. (for example: hobbies, work, pets etc.)", value=decrypt(nineth), inline=False)
+				embed.add_field(name="What was rule number 5 in our server rules section?", value=decrypt(tenth), inline=False)
+
+	        	await channel.send(embed=embed)
+
+watcher.start()
+
 @bot.event
 async def on_ready():
 	activity = disnake.Game(name="Have you applied?")
