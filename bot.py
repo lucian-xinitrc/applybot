35e43bac1e4cf51f86c8022c9efd0c814c3356d7
lucian-xinitrc <cusmirlucian@gmail.com>
2026-02-21 16:03:41 +0000

    New commit

--- bot.py
+++ bot.py
@@ -24,8 +24,6 @@ class ResponseApplyEmbed(disnake.ui.View):
 		)
 		if member:
 			await interaction.response.send_message(f"{member.mention} You've been accepted!", ephemeral=True)
-		else:
-			await interaction.response.send_message(f"{self.username} couldn't be found!", ephemeral=True)
 
     @disnake.ui.button(label="Deny", style=disnake.ButtonStyle.danger, custom_id="deny_btn")
     async def button_deny_callback(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
